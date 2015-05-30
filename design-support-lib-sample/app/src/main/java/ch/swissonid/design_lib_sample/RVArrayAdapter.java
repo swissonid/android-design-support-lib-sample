1f6ed68c3f7fcfa6f313274fcf9b4e5e687e3f23
Patrice Mueller <mueller.patrice@gmail.com>
2015-05-30 13:27:58 +0200

    Init commit

---
+++ design-support-lib-sample/app/src/main/java/ch/swissonid/design_lib_sample/RVArrayAdapter.java
@@ -0,0 +1,40 @@
+package ch.swissonid.design_lib_sample;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class RVArrayAdapter extends RecyclerView.Adapter<RVArrayAdapter.ViewHolder> {
+
+    private String[] mData;
+    public RVArrayAdapter(String[] data){
+        mData = data;
+    }
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View view = LayoutInflater
+                .from(viewGroup.getContext())
+                .inflate(android.R.layout.simple_list_item_1,viewGroup,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int position) {
+        viewHolder.mTextView.setText(mData[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.length;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public TextView mTextView;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            mTextView = (TextView)itemView;
+        }
+    }
+}
