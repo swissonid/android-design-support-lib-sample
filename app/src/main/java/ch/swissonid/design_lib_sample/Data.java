2e7069fa2e894b1a3829daa88a9dd79c4c1c5e04
Patrice Mueller <mueller.patrice@gmail.com>
2015-09-19 14:20:10 +0200

    added Flixble White Space with overlapping content

---
+++ app/src/main/java/ch/swissonid/design_lib_sample/Data.java
@@ -0,0 +1,7 @@
+package ch.swissonid.design_lib_sample;
+
+/**
+ * Created by pmueller on 19.9.15.
+ */
+public class Data {
+}
---
+++ app/src/main/java/ch/swissonid/design_lib_sample/fragments/OverlappingContentFragment.java
@@ -0,0 +1,7 @@
+package ch.swissonid.design_lib_sample.fragments;
+
+/**
+ * Created by pmueller on 19.9.15.
+ */
+public class OverlappingContentFragment {
+}
